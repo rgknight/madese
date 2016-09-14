c50f75d69999d44bedce0704af5ef992b4e3828f
Ryan Knight <ryangknight@gmail.com>
2016-09-14 09:31:29 -0400

    add ssdr

--- R/dese_get.R
+++ R/dese_get.R
@@ -1,5 +1,3 @@
-require(dplyr)
-
 #' Get data from Massachusetts DESE
 #'
 #' Get public school information from Massachusetts \href{http://profiles.doe.mass.edu/state_report/}{State Reports}.
@@ -142,6 +140,125 @@ dese_attrition <- function(year, mode='SCHOOL', group='ALL'){
 
   result <- result %>%
     mutate_at(vars(3:15),funs(gsub(" ", "", .)))
+
+  result
+}
+
+
+#' Mobility Rates
+#'
+#' Get the \href{http://profiles.doe.mass.edu/state_report/mobilityrates.aspx}{Mobility Rates}
+#' Report from Massachusetts DESE website.
+#' Mobility rates has various metrics on how many students move through the school in a year
+#'
+#' @param year the year of the report
+#' @param mode district or school
+#' @param group get the report for a specific student group. Defaults to all students.
+#' @seealso \code{\link{dese_get}} which this function wraps
+#' @import rvest
+#' @importFrom magrittr extract2
+#' @importFrom xml2 read_html
+#' @export
+#' @examples
+#' dese_mobilityrates(2016)
+dese_mobilityrates <- function(year, mode='SCHOOL', group='ALL'){
+  query <- list(
+    'ctl00$ContentPlaceHolder1$reportType'=toupper(mode),
+    'ctl00$ContentPlaceHolder1$cohortYear'=as.character(year),
+    'ctl00$ContentPlaceHolder1$studentGroup'=toupper(group),
+    'ctl00$ContentPlaceHolder1$rateType'='4-Year:REG',
+    'export_excel'='yes'
+  )
+
+  parsed <- dese_get('mobilityrates', query)
+
+  result <- read_html(parsed) %>%
+    html_nodes('table') %>%
+    extract2(2) %>%
+    html_table(header = T, fill=T)
+
+  result
+}
+
+#' dese_ssdr_days_missed
+#'
+#' Get SSDR-based reports from Massachusetts DESE website.
+#'
+#' @param year the year of the report
+#' @param mode district or school
+#' @param group get the report for a specific student group. Defaults to all students. Can be
+#' - ALL = All students
+#' - SPED = Students with disabilities
+#' - ECODIS = Economically disadvantaged students
+#' - F = Female students
+#' - M = Male students
+#' - AI = American Indian or Alaskan Native students
+#' - AS = Asian students
+#' - BL = Black / African American students
+#' - HS = Hispanic / Latino students
+#' - MR = Multiracial, non-hispanic / Latino students
+#' - HP = Native Hawaiian / Pacific Islander students
+#' - WH = White students
+#' @param offense Defaults to all offenses.
+#' @seealso \code{\link{dese_get}} which this function wraps
+#' @import rvest
+#' @importFrom magrittr extract2
+#' @importFrom xml2 read_html
+#' @export
+dese_ssdr_days_missed <- function(year, mode='SCHOOL', group='ALL', offense='All'){
+  query <- list(
+    'ctl00$ContentPlaceHolder1$reportType'=toupper(mode),
+    'ctl00$ContentPlaceHolder1$fycode'=as.character(year),
+    'ctl00$ContentPlaceHolder1$studentGroup'=toupper(group),
+    'ctl00$ContentPlaceHolder1$subjectCode'=toupper(offense),
+    'export_excel'='yes'
+  )
+
+  parsed <- dese_get('ssdr_days_missed', query)
+
+  result <- read_html(parsed) %>%
+    html_nodes('table') %>%
+    extract2(2) %>%
+    html_table(header = T, fill=T)
+
+  # Take out unicode pad character
+  # Unfortunately due to https://github.com/hadley/dplyr/issues/1978
+  # we can't just mutate_if(is.character)
+
+  result <- result %>%
+    mutate_all(funs(gsub(" ", "", .)))
+
+  result
+}
+
+#' @rdname dese_ssdr_days_missed
+#' @import rvest
+#' @importFrom magrittr extract2
+#' @importFrom xml2 read_html
+#' @export
+dese_ssdr <- function(year, mode='SCHOOL', group='ALL', offense='All'){
+  query <- list(
+    'ctl00$ContentPlaceHolder1$reportType'=toupper(mode),
+    'ctl00$ContentPlaceHolder1$fycode'=as.character(year),
+    'ctl00$ContentPlaceHolder1$studentGroup'=toupper(group),
+    'ctl00$ContentPlaceHolder1$subjectCode'=toupper(offense),
+    'export_excel'='yes'
+  )
+
+  parsed <- dese_get('ssdr', query)
+
+  if (grepl("No Data Exists for this search criteria", parsed)){
+    warning(sprintf("No Data Exists for %s %s %s %s", as.character(year), mode, group, offense))
+    return(1)
+  }
+
+  result <- read_html(parsed) %>%
+    html_nodes('table') %>%
+    extract2(2) %>%
+    html_table(header = T, fill=T)
+
+  result <- result %>%
+    mutate_all(funs(gsub(" ", "", .)))
 
   result
 }
